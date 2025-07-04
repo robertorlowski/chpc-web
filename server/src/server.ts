2a8ef02162d39d80656d5bbd494f17182dfdf909
Robert Orłowski <robson.orlowski@gmail.com>
2025-07-04 12:37:56 +0200

    a

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,5 +3,5 @@ import app from './middleware/app'
 const port = 10000
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
+  console.log(`App listening at https://chpc-web.onrender.com:${port}`)
 })
