14b59c58beacc76026236834c9de72633a673559
Robert Orłowski <robson.orlowski@gmail.com>
2025-07-04 22:31:23 +0200

    .

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,10 @@
 import app from './middleware/app'
 const port = process.env.PORT || 3001;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App listening at https://chpc-web.onrender.com:${port}`)
 })
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
+
