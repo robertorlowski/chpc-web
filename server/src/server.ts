a8744e2e78678d406e8c2f17f4bc40a467750e86
Robert Orłowski <robson.orlowski@gmail.com>
2025-07-04 22:59:26 +0200

    .

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,11 @@
-
 import 'dotenv/config';
 import app from './middleware/app'
+
 
 const port = process.env.PORT || 3001;
 
 const server = app.listen(port, () => {
-  console.log(`Example app listening on port: ${port}`)
+  console.log(`${port}`)
 })
 
 server.keepAliveTimeout = 120 * 1000;
