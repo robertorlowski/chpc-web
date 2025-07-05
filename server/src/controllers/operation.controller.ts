790cff4e616c413a8e0c4ad84a484109a3d137b7
Robert Orłowski <robson.orlowski@gmail.com>
2025-07-06 01:33:57 +0200

    q

--- server/src/controllers/operation.controller.ts
+++ server/src/controllers/operation.controller.ts
@@ -1,11 +1,25 @@
 import { Request, Response } from 'express'
 import { getOperationData, setOperationData } from '../services/operation.service';
 import { TOperationCO } from '../middleware/type';
+import { getHpLastData } from '../services/hp.service';
 
 
 export async function getOperation(req: Request, res: Response) {
   try {
-    return res.status(200).send(getOperationData())
+
+    const data = await getHpLastData()
+    const op :TOperationCO = {};
+    op.force = data.HP?.F ? "1" :"0";
+    op.co_min = data?.co_min;
+    op.co_max = data?.co_max;
+    op.cwu_min = data?.cwu_min,
+    op.cwu_max = data?.co_max,
+    op.cold_pomp = data?.HP?.CCS ? "1" : "0";
+    op.hot_pomp = data?.HP?.HCS ? "1" : "0";
+    op.sump_heater = data?.HP?.SHS ? "1" : "0";
+    op.work_mode = data?.work_mode;
+ 
+    return res.status(200).send(op);
   } catch (error) {
     return res.status(500).send({ error: error })
   }
@@ -16,3 +30,4 @@ export const setOperation = async (req: Request<{}, {}, TOperationCO>, res: Resp
   setOperationData(op)
   return res.status(201).json({ message: op });
 }
+
--- server/src/services/hp.service.ts
+++ server/src/services/hp.service.ts
@@ -30,18 +30,6 @@ export const getHpAllData= async () => {
 export const addHpData = async (data :TCO) => {
   await db.read();
   const coList: TCO[] = db.data.hp;
-  const op :TOperationCO = {};
-  op.force = data.HP?.F ? "1" :"0";
-  op.co_min = data?.co_min;
-  op.co_max = data?.co_max;
-  op.cwu_min = data?.cwu_min,
-  op.cwu_max = data?.co_max,
-  op.cold_pomp = data?.HP?.CCS ? "1" : "0";
-  op.hot_pomp = data?.HP?.HCS ? "1" : "0";
-  op.sump_heater = data?.HP?.SHS ? "1" : "0";
-  op.work_mode = data?.work_mode;
-  setOperationData(op);
-
   //jeśli spręzarka ne działa i poprzednio nie działała to nie zapisujemy danych do bazy
   if (!data?.HP?.HPS) {
     if (coList.length > 0 && !coList[coList.length -1].HP?.HPS ) {
