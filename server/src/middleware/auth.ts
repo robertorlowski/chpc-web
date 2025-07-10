8d424307808d2fb51237672aad697ea729606869
Robert Orłowski <robson.orlowski@gmail.com>
2025-07-11 01:25:55 +0200

    .

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -7,15 +7,15 @@ dotenv.config();
 const API_KEY = process.env.API_KEY || 'f3c87b02-4d0d-4e0a-9d5c-30a91ec77510';
 
 export function verifyApiKey(req: Request, res: Response, next: NextFunction): void {
-  if (req.method === 'OPTIONS') {
-    return next();
-  }
+  // if (req.method === 'OPTIONS') {
+  //   return next();
+  // }
 
-  const apiKey = req.header('x-api-key');
-  if (!apiKey || apiKey !== API_KEY) {
-    res.status(403).json({ error: 'Forbidden: Invalid API Key' });
-    return;
-  }
+  // const apiKey = req.header('x-api-key');
+  // if (!apiKey || apiKey !== API_KEY) {
+  //   res.status(403).json({ error: 'Forbidden: Invalid API Key' });
+  //   return;
+  // }
 
   next();
 }
