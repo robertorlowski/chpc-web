8442c8542415ce989ac5191f96950715f6baef44
Robert Orłowski <robson.orlowski@gmail.com>
2025-07-20 18:31:58 +0200

    ws

---
+++ server/src/middleware/webSocet.ts
@@ -0,0 +1,39 @@
+import WebSocket, { WebSocketServer } from 'ws';
+
+let espClients: Set<WebSocket> = new Set();
+
+export const createWsServer = (server: any) => {
+  const wss = new WebSocketServer({ server });    
+  
+  console.log("server create");
+
+
+  wss.on('connection', (ws: WebSocket, req) => {
+    const ip = req.socket.remoteAddress;
+    console.log(`New client connected: ${ip}`);
+    espClients.add(ws);
+
+    ws.on('message', (message) => {
+      console.log(`Received from client [${ip}]:`, message.toString());
+    });
+
+    ws.on('close', () => {
+      console.log(`Client disconnected: ${ip}`);
+      espClients.delete(ws);
+    });
+  });
+
+}
+
+export const sendMessage = (message: String) => {
+ if (!message) {
+    return;
+  }
+  let sent = 0;
+  espClients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      console.log("send - opperation")
+      client.send(message);
+    }
+  });
+}
