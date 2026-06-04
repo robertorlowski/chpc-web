2ef38a751350376ff2ebb055927325c5fc972362
Robert Orłowski <robson.orlowski@gmail.com>
2026-06-04 14:34:48 +0200

    fix

--- server/src/middleware/openmeteo.ts
+++ server/src/middleware/openmeteo.ts
@@ -12,9 +12,9 @@ const url = "https://api.open-meteo.com/v1/forecast";
 let temperature_2m :number | null | undefined;
 
 export const prepareMeteoData = async () => {
-	const responses = await fetchWeatherApi(url, params);
-	const current = responses[0].current()!;
-	temperature_2m =  Math.trunc(current.variables(0)?.value()! * 10) / 10;
+	// const responses = await fetchWeatherApi(url, params);
+	// const current = responses[0].current()!;
+	// temperature_2m =  Math.trunc(current.variables(0)?.value()! * 10) / 10;
 	// console.log(temperature_2m)
 }
 
